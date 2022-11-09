5ccd2d9d2dcff2cbc30aa5950ff03b543b471573
Alvaro <alvaro.fernandez@iesnervion.es>
2022-11-09 15:39:50 +0100

    Initial commit

---
+++ Assets/Scripts/SongManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    public static SongManager instance;
+    private AudioSource songAudio;
+    private float songDelay; //delay in seconds //tf is this
+    private int inputDelay; //in milliseconds
+
+    private string fileLocation; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
